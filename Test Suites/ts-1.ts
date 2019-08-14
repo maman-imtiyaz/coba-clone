e5203a2e8cbe47f7431ca0a7ac50c2206c61623f
LENOVO <LENOVO@Rahman>
2019-08-14 14:15:09 +0700

    add tc-6

---
+++ Test Suites/ts-1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ts-1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>263203f9-910e-48fb-847a-474a122cfe27</testSuiteGuid>
+   <testCaseLink>
+      <guid>3d4a28cb-73ef-40a3-a7f5-19df98d4c87b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc-1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
